56a381ff0484d7ac51f7f02244f3e0cf02e00ed9
Chuka <g00414637@atu.ie>
2023-10-23 09:57:54 +0100

    added student details to Person class

--- src/main/java/ie/atu/Main.java
+++ src/main/java/ie/atu/Main.java
@@ -6,5 +6,6 @@ public class Main {
     public static void main(String[] args) {
         Person student1 = new Person("Chuka","Orajuba",18);
         System.out.println("Details are" + " " + student1.getFirstName());
+
     }
 }
--- src/main/java/ie/atu/Person.java
+++ src/main/java/ie/atu/Person.java
@@ -45,4 +45,5 @@ public class Person {
     public void setAge(int age) {
         this.age = age;
     }
+
 }
